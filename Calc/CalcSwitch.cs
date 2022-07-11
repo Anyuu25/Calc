17423ae840a5c228429fff3ff6245b8ab4dc6479
Anyuu25 <anyuu25@gmail.com>
2022-07-11 22:35:01 +0500

    first commit

---
+++ Calc/CalcSwitch.cs
@@ -0,0 +1,52 @@
+﻿//с оператором switch
+while (true)
+{
+    Console.Clear();
+    double firstValue, secondValue;
+    string operation;
+
+    try
+    {
+        Console.WriteLine("Введите число 1");
+        firstValue = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите число 2");
+        secondValue = double.Parse(Console.ReadLine());
+    }
+        catch (Exception)
+    {
+        Console.WriteLine("Не удалось преобразовать строку в число. Для новой попытки нажмите Enter");
+        Console.ReadLine();
+        continue;
+    }
+    Console.WriteLine("Выберите операцию над числами: '+' '-' '*' '/' ");
+    operation = Console.ReadLine();
+
+    switch (operation)
+    {
+        case "+":
+            Console.WriteLine(firstValue + secondValue);
+            break;
+        case "-":
+            Console.WriteLine(firstValue - secondValue);
+            break;
+        case "*":
+            Console.WriteLine(firstValue * secondValue);
+            break;
+        case "/":
+            if (secondValue == 0)
+            {
+                Console.WriteLine("Ошибка! На ноль делить нельзя! Для новой попытки нажмите Enter");
+            }
+            else
+            {
+                Console.WriteLine(firstValue / secondValue);
+            }
+            break;
+
+        default:
+            Console.WriteLine("Ошибка! Неизвестное действие! Для новой попытки нажмите Enter");
+            break;
+    }
+    Console.ReadLine();
+}
---
+++ calcIfElse/CalcIfElse.cs
@@ -0,0 +1,36 @@
+﻿double firstValue, secondValue;
+string operation;
+
+Console.WriteLine("Введите число 1");
+firstValue = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 2");
+secondValue = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Выберите операцию над числами: '+' '-' '*' '/' ");
+operation = Console.ReadLine();
+
+if (operation == "+")
+{
+    Console.WriteLine(firstValue + secondValue);
+}
+else if (operation == "-")
+{
+    Console.WriteLine(firstValue - secondValue);
+}
+else if (operation == "*")
+{
+    Console.WriteLine(firstValue * secondValue);
+}
+else if (operation == "/")
+{
+    if (secondValue == 0)
+        Console.WriteLine("Ошибка! На ноль делить нельзя!");
+    else
+    Console.WriteLine(firstValue / secondValue);
+}
+else
+{
+    Console.WriteLine("Ошибка! Неизвестной действие!");
+}
+Console.ReadLine();
